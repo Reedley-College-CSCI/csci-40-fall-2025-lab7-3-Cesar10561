4cbed33dbe7b1be67ffc65af5b7bf854b9deec4b
Cesar Duarte <0991311@MY.SCCCD.EDU>
2025-10-14 19:25:55 -0700

    Created TemperatureRecord struct to store day and temperature

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -1,28 +1,37 @@
+// Cesar Duarte
+// CSCI-40 Part 3 Lab
+
+
+
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 // TODO: Step 1 - Define the struct TemperatureRecord
 // It should contain two integer fields: day and temperature.
-
+struct TemperatureRecord {
+    int day;
+    int temperature;
+};
 
 // Constants
 const int MAX_DAYS = 31;
 
 // Function Prototypes
-void readTemperatures(???); // TODO: Fix the parameters
-void printTemperatures(const ???);
-TemperatureRecord findMin(const ???);
-TemperatureRecord findMax(const ???);
-double findAverage(const ???);
+void readTemperatures(TemperatureRecord records[], int numRecords); // TODO: Fix the parameters
+void printTemperatures(const TemperatureRecord records[], int numRecords);
+TemperatureRecord findMin(const TemperatureRecord records[], int numRecords);
+TemperatureRecord findMax(const TemperatureRecord records[], int numRecords);
+double findAverage(const TemperatureRecord records[], int numRecords);
 
 int main() {
     // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
-    
+   // TemperatureRecord records[MAX_DAYS];
+
     int size = 0;  // Actual number of records read
 
     // TODO: Step 3 - Call readTemperatures() to load data from file
-
+    
     // TODO: Step 4 - Print the temperatures
 
     // TODO: Step 5 - Compute and display min, max, and average temperature
